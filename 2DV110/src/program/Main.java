c65593a34ffbc5e33d080aede1c48b0d1b30c694
Johan Widén <jw222hk@student.lnu.se>
2014-11-09 20:42:18 +0100

    Testing to commit with GIT.

---
+++ 2DV110/src/program/Main.java
@@ -0,0 +1,13 @@
+package program;
+
+/**
+ * This class i just used for trying and setting up the git repository. 
+ * @author Johan Widen
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
