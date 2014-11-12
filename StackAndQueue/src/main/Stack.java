aef9e76682bc9870fc47aac825e3bfd55a60147b
Johan Widén <jw222hk@student.lnu.se>
2014-11-12 16:21:14 +0100

    Green - push implemented.

--- StackAndQueue/src/main/Stack.java
+++ StackAndQueue/src/main/Stack.java
@@ -1,11 +1,24 @@
 package main;
 
+import java.util.LinkedList;
+
 /**
  * This class is our implementation of a Stack.
  * 
  * @author Johan Widen
  * 
  */
-public class Stack {
+public class Stack<E> {
+
+    private LinkedList<E> stack;
+
+    public Stack() {
+        stack = new LinkedList<E>();
+    }
+
+    public void push(E element) {
+        stack.addFirst(element);
+    }
+
 
 }
