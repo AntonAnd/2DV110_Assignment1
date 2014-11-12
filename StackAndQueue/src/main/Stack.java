5ff706ed0fe681bd701f837a646c7c84c7d4006a
Johan Widén <jw222hk@student.lnu.se>
2014-11-12 14:25:33 +0100

    REFACTOR - Changed javaDoc some, mostly to test new way of commiting...

--- StackAndQueue/src/main/Stack.java
+++ StackAndQueue/src/main/Stack.java
@@ -1,18 +1,9 @@
 package main;
 
 /**
- * This class is our implementation of a Stack with following operations and
- * demands:
+ * This class is our implementation of a Stack.
  * 
- * – push an element
- * 
- * – pop an element
- * 
- * – peek the top element
- * 
- * – return stack size
- * 
- * @author Johan Widén
+ * @author Johan Widen
  * 
  */
 public class Stack {
