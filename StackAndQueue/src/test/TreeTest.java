7415f5ed0319e0f1169f04e4bd7e25a0319266d2
Johan Widén <jw222hk@student.lnu.se>
2014-11-20 12:31:55 +0100

    RED - Created TreeTest and try to create a Tree instance.

---
+++ StackAndQueue/src/test/TreeTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Johan Widen on 2014-11-20.
+ */
+public class TreeTest<E> {
+
+    @Test
+    public void testConstructor(){
+       Tree<String> a = new Tree<String>();
+    }
+}
