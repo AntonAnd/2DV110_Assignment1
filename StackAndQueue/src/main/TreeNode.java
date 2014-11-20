4fba8df2aae9deb10ffe58e1f80525d2be0acb46
Anton <tonan91@gmail.com>
2014-11-20 10:56:07 +0100

    RED - Created classes, and added test for get children.

---
+++ StackAndQueue/src/main/TreeNode.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.ArrayList;
+
+public class TreeNode<E> {
+
+	TreeNode<E> parent;
+	ArrayList<TreeNode<E>> children;
+	E data;
+	
+	
+}
---
+++ StackAndQueue/src/test/TreeNodeTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import main.TreeNode;
+
+import org.junit.Test;
+
+public class TreeNodeTest<E> {
+
+	
+	@Test
+	public void getChildrenTest(){
+		TreeNode<String> t = new TreeNode<String>();
+
+	}
+	
+}
