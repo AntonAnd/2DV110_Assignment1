580bb76015a928a3f06c8a1ffa432e7e12a8644e
Anton <tonan91@gmail.com>
2014-11-24 21:04:14 +0100

    REFACTOR - added javadoc to QueueClass.

--- StackAndQueue/src/main/QueueClass.java
+++ StackAndQueue/src/main/QueueClass.java
@@ -3,15 +3,27 @@ package main;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * This class represents the queue ADT.
+ * @author Anton
+ *
+ * @param <E>
+ */
 public class QueueClass<E> {
 	
 	private Queue<E> queue;
 	
+	/**
+	 * QueueClass constructor.
+	 */
 	public QueueClass(){
 		queue = new LinkedList<E>();
 	}
-	
-	/*Enqueue an element*/
+
+	/**
+	 * Enqueues an element to the queue.
+	 * @param element element to be added.
+	 */
 	public void enqueue(E element){
 		if(element == null){
 			throw new IllegalArgumentException();
@@ -19,19 +31,27 @@ public class QueueClass<E> {
 		queue.add(element);
 	}
 	
-	/*Returns the first element in the queue*/
+	/**
+	 * Returns the first element in the queue, without removing it.
+	 * @return head of queue.
+	 */
 	public E peek(){
 		return queue.peek();
 	}
 	
-	/*Returns and removes the head of the queue, or null if
+	/**
+	 * Returns and removes the head of the queue, or null if 
 	 * queue is empty.
+	 * @return head of queue.
 	 */
 	public E dequeue() {
 		return queue.poll();
 	}
-	
-	/*Returns the size of the queue*/
+
+	/**
+	 * Returns the size of the queue.
+	 * @return size of the queue.
+	 */
 	public int getSize(){
 		return queue.size();
 	}
