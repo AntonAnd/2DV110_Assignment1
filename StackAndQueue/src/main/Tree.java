1aa5e139194d5b50368788a1ea9ab9ba23034794
Johan Widén <jw222hk@student.lnu.se>
2014-11-24 15:28:47 +0100

    GREEN - Check for null parent.

--- StackAndQueue/src/main/Tree.java
+++ StackAndQueue/src/main/Tree.java
@@ -39,7 +39,11 @@ public class Tree<E> {
      * @param child the TreeNode that we add to the parent.
      */
     public void addChild(TreeNode<E> parent, TreeNode<E> child) {
-        parent.getChildren().add(child);
+        if (parent == null) {
+            root.getChildren().add(child);
+        } else {
+            parent.getChildren().add(child);
+        }
         size++;
     }
 }
